cb3c3cdea7d32ad17beaa3a4db7cab564e0b3b7c
Ferdous-Al-Imran <1205081.mzfs@ugrad.cse.buet.ac.bd>
2016-06-07 11:08:17 +0600

    final commit

--- website/website/sensorValue/tests.py
+++ website/website/sensorValue/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import User,ParameterList,ValueEntry,ParameterIdeals
 
-# Create your tests here.
+
+class CreateUser(TestCase):
+	def setUp(self):
+		User.objects.create(username="Nazrul Islam",password="password1")
+		User.objects.create(username="Anjan Bashak",password="password2")
+		# User.objects.create(username="Nazrul Islam",password="password3")
+
+
+	def test_user_is_created(self):
+		user1=User.objects.get(username="Nazrul Islam")
+		self.assertEqual(user1.username,"Nazrul Islam")
+
+	
+	def test_password_matches(self):
+		user1=User.objects.get(username="Nazrul Islam")
+		self.assertEqual(user1.password,"password1")
+
+
+
+
+
+
+
+
+
+
+
+
+
